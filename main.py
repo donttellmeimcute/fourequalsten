11b6332631b5d3d875692dcd277338dcb38f26d1
natsuki <24362190+donttellmeimcute@users.noreply.github.com>
2022-12-17 23:04:24 -0600

    just execute the following command: python main.py

---
+++ main.py
@@ -0,0 +1,154 @@
+import itertools
+
+def main():
+    #input numbers and put them in a list
+    numbers = []
+    for i in range(4):
+        try:
+            numbers.append(int(input("Numero"+str(i+1)+": ")))
+        except ValueError:
+            print("Error: Solo se aceptan numeros")
+            exit()
+    operations = ['+','-','*','/']
+    for i in itertools.product(operations, repeat=3):
+        for j in itertools.permutations(numbers):
+            a = j[0]
+            b = j[1]
+            c = j[2]
+            d = j[3]
+            op1 = i[0]
+            op2 = i[1]
+            op3 = i[2]
+            if op1 == '+':
+                if op2 == '+':
+                    if op3 == '+':
+                        try:
+                            if a + b + c + d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                    if op3 == '-':
+                        try:
+                            if a + b + c - d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                    if op3 == '*':
+                        try:
+                            if a + b + c * d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+
+                    if op3 == '/':
+                        try:
+                            if a + b + c / d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                if op2 == '-':
+                    if op3 == '+':
+                        try:
+                            if a + b - c + d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                    if op3 == '-':
+                        try:
+                            if a + b - c - d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                    if op3 == '*':
+                        try:
+                            if a + b - c * d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                    if op3 == '/':
+                        try:
+                            if a + b - c / d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                if op2 == '*':
+                    if op3 == '+':
+                        try:
+                            if a + b * c + d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                    if op3 == '-':
+                        try:
+                            if a + b * c - d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                    if op3 == '*':
+                        try:
+                            if a + b * c * d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                    if op3 == '/':
+                        try:
+                            if a + b * c / d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                if op2 == '/':
+                    if op3 == '+':
+                        try:
+                            if a + b / c + d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                    if op3 == '-':
+                        try:
+                            if a + b / c - d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                    if op3 == '*':
+                        try:
+                            if a + b / c * d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                    if op3 == '/':
+                        try:
+                            if a + b / c / d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+            if op1 == '-':
+                if op2 == '+':
+                    if op3 == '+':
+                        try:
+                            if a - b + c + d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                    if op3 == '-':
+                        try:
+                            if a - b + c - d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                    if op3 == '*':
+                        try:
+                            if a - b + c * d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                    if op3 == '/':
+                        try:
+                            if a - b + c / d == 10:
+                                print(a,op1,b,op2,c,op3,d)
+                        except ZeroDivisionError:
+                            pass
+                
+
+if __name__ == "__main__":
+    while True:
+        main()
